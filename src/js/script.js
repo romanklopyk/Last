6f79399e6cbfa290ebf4fb9a4afe3a4c8efd0093
romanklopyk <86305348+romanklopyk@users.noreply.github.com>
2021-08-12 21:27:36 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,20 @@ const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 1,
+
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+
     },
 
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+
     },
 
     // And if we need scrollbar
@@ -19,3 +23,8 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-scrollbar',
     },
 });
+
+/* const swiper = document.querySelector('.swiper-container').swiper;
+
+// Now you can use all slider methods like
+swiper.slideNext(); */
